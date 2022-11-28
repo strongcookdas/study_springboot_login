4c38fe00dd27f3faec13b99a456e0473960e187d
strongcookdas <goodjav141592@gmail.com>
2022-11-28 17:38:18 +0900

    build : start springboot login project

---
+++ src/test/java/com/login/study_springboot_login/StudySpringbootLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.study_springboot_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
